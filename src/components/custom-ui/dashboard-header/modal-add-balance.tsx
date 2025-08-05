261ef1c8bb92a00186384171e0ba60750c6f394c
Matheus Rafael <matheusrafael77@hotmail.com>
2025-08-05 16:44:53 -0300

    style(bignumbers) corrige bignumbers para celular 1 coluna

---
+++ src/components/custom-ui/dashboard-header/modal-add-balance.tsx
@@ -0,0 +1,40 @@
+
+
+export default function modalAddBalance() {
+
+
+//  <Dialog open={open} onOpenChange={setOpen}>
+//       <DialogContent>
+//         <DialogHeader>
+//           <DialogTitle>Entrar como visitante</DialogTitle>
+//           <DialogDescription>
+//             Para continuar sem autenticação, informe um nome de usuário. Seus
+//             dados serão salvos apenas no seu navegador e não serão enviados para
+//             o servidor.
+//           </DialogDescription>
+//         </DialogHeader>
+//         <DialogFooter>
+//           <Form {...form}>
+//             <form onSubmit={form.handleSubmit(submit)} className="flex gap-3">
+//               <div>
+//                 <FormField
+//                   control={form.control}
+//                   name="name"
+//                   render={({ field }) =>
+//                     Input({
+//                       ...field,
+//                       placeholder: "Nome de usuário",
+//                     })
+//                   }
+//                 />
+//                 <FormMessage>
+//                   {form.formState.errors?.name?.message}
+//                 </FormMessage>
+//               </div>
+//               <Button="submit">Entrar</Button>
+//             </form>
+//           </Form>
+//         </DialogFooter>
+//       </DialogContent>
+//     </Dialog>
+}
