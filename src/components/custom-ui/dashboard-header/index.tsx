c1b7bc90dab0d08b0f26b1410dd744f276abbef1
Matheus Rafael <matheusrafael77@hotmail.com>
2025-08-05 00:58:59 -0300

    feat adiciona mensagem de boas vindas

--- src/components/custom-ui/dashboard-header/index.tsx
+++ src/components/custom-ui/dashboard-header/index.tsx
@@ -1,4 +1,4 @@
-import { Card, CardAction, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardAction, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { useAuthStore } from "@/context/useAuthStore";
 import DropdownMenuUser from "./dropdown-menu-user";
@@ -9,6 +9,7 @@ export default function DashboardHeader() {
     <Card className="@container/card-header rounded-none">
       <CardHeader>
         <CardTitle>Olá, {user?.name}</CardTitle>
+        <CardDescription>Seja bem-vindo(a)</CardDescription>
         <CardAction>
           <DropdownMenuUser/>
         </CardAction>
